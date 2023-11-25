a9831459bbe6b84acfcb56d448cbd336451d998b
ChoHangMin <slightsmille@gmail.com>
2023-11-25 10:07:17 +0900

    11

--- app/src/main/java/com/example/omok/MainActivity.java
+++ app/src/main/java/com/example/omok/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         leaderBoardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LeaderboardActivity.class);
+                Intent intent = new Intent(MainActivity.this, VictoryActivity.class);
                 startActivity(intent);
 
             }
